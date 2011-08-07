13072a63a415e6877011c91791e7efd190577a54
Akvel <akvel@inbox.ru>
2011-08-07 22:47:39 +0600

    test

---
+++ Test/src/TestServlet.java
@@ -0,0 +1,14 @@
+
+public class TestServlet {
+
+
+
+		    public static void main(String[] args) throws Exception {   
+		        System.out.println(
+		        		TestServlet.class.getClassLoader().getResource("javax/servlet/http/HttpServlet.class"));
+		    }
+		
+
+	
+
+}
