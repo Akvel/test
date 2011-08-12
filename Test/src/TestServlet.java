519f81651a2e24d3f2532bac1713bb4662ec9e5a
akvel <Akincev_VA@ava_test>
2011-08-12 09:10:46 +0600

    gfjfg

--- Test/src/TestServlet.java
+++ Test/src/TestServlet.java
@@ -6,6 +6,9 @@ public class TestServlet {
 		    public static void main(String[] args) throws Exception {   
 		        System.out.println(
 		        		TestServlet.class.getClassLoader().getResource("javax/servlet/http/HttpServlet.class"));
+		        
+		        
+		        Test
 		    }
 		
 
